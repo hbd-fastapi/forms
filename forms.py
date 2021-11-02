4ade99dcdc8815e80ae11b46b1a9b676a98cf25a
Maciej Sitowski <maciej.sitowski@lingarogroup.com>
2021-11-02 08:00:48 +0100

    Add endpoint as a form

--- forms.py
+++ forms.py
@@ -4,8 +4,13 @@ In this tutorial I will learn how to use fastapi forms
 
 """
 
-from fastapi import FastAPI
+from fastapi import FastAPI, Form
 
+app = FastAPI()
+
+@app.post("/language/")
+async def language(name: str = Form(...), type: str = Form(...)):
+    return {'name': name, "type": type}
 
 
 
